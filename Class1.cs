43d4de0a73f08d1dd71218a0e5cb00eb8ee7337f
Staler2019 <abcde121234@gmail.com>
2024-09-07 15:28:23 +0800

    Chore: add WriteToDocx()

--- Class1.cs
+++ Class1.cs
@@ -1,9 +1,37 @@
-﻿namespace CSharpIntoPython;
+﻿using System.Text;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace CSharpIntoPython;
 
 public static class Class1
 {
     public static string HelloWorld()
     {
         return "Hello World!";
+    }
+
+    public static void WriteToDocx(string fileFullPath, string strToWrite)
+    {
+        using (var doc = WordprocessingDocument.Open(fileFullPath, true))
+        {
+            var altChunkId = "cid";
+            var mainDocPart = doc.MainDocumentPart;
+            var body = mainDocPart.Document.Body;
+
+            var inject = $"<p>{strToWrite}</p>";
+            var injectStr = $"<html><head></head><body>{inject}</body></html>";
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(injectStr));
+            var importPart = mainDocPart.AddAlternativeFormatImportPart(
+                AlternativeFormatImportPartType.Html
+                , altChunkId);
+            importPart.FeedData(ms);
+
+            var altChunk = new AltChunk();
+            altChunk.Id = altChunkId;
+
+            body.Append(altChunk);
+        }
     }
 }
