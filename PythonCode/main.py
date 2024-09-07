5e2f5a259fe97b7912ffd53e3b6fa0c7488cef0e
Staler2019 <abcde121234@gmail.com>
2024-09-07 16:30:21 +0800

    Chore: import namespace and use function alias

--- PythonCode/main.py
+++ PythonCode/main.py
@@ -17,3 +17,10 @@ publishPath = os.path.join(os.getcwd(),
                            "win-x64")
 dllPath = os.path.join(publishPath, "CSharpIntoPython.dll")
 clr.AddReference(dllPath)
+
+
+# from namespace import class
+from CSharpIntoPython import Class1
+
+hello_world = Class1.HelloWorld
+write_to_docx = Class1.WriteToDocx
