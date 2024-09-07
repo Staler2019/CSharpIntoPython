e04fab01fac7b3c55c2bf0480423783e135af668
Staler2019 <abcde121234@gmail.com>
2024-09-07 16:29:37 +0800

    Chore: load dll

--- PythonCode/main.py
+++ PythonCode/main.py
@@ -2,3 +2,18 @@ import pythonnet
 
 pythonnet.load("coreclr")
 # print(pythonnet.get_runtime_info())
+
+
+import os
+import clr
+
+publishPath = os.path.join(os.getcwd(),
+                           "..",
+                           "CSharpLib",
+                           "bin",
+                           "Release",
+                           "net6.0",
+                           "publish",
+                           "win-x64")
+dllPath = os.path.join(publishPath, "CSharpIntoPython.dll")
+clr.AddReference(dllPath)
